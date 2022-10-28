f8bfa2d4b0d7891655fd8c2ae10da8a746c48a01
AyeMyatMyatKhine19 <ayemyat.myatkhine@seattleconsultingmyanmar.com>
2022-10-28 09:56:48 +0630

    install nuxtjs project

--- frontend/src/pages/post/list.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="container">
-        <div class="search_item">
-            <div class="btn btn-primary">Create</div>
-        </div>
-    </div>
-</template>
-
-<script src="../../services/post/list.js"></script>
